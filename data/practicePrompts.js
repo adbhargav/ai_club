9451ddd04c5b3005e2bfd2206086707f4f82dbdc
BHARGAV A <bhargavandhe202@gmail.com>
2025-07-29 06:50:24 +0530

    Add files via upload

---
+++ data/practicePrompts.js
@@ -0,0 +1,15 @@
+const practicePrompts = [
+  {
+    id: "1",
+    type: "image",
+    title: "Futuristic City",
+    description: "	Noir	&	Detective-Inspired	Portrait	Prompts.",
+    defaultPrompt: "A futuristic city glowing with neon lights and flying cars",
+    mediaUrl: "/images/future-city.jpg",
+    category: "Sci-Fi",
+    difficulty: "Intermediate"
+  },
+ 
+];
+
+export default practicePrompts;
